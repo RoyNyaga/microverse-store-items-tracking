71798d35644be4060488400f471ecda58b71daee
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-03 19:36:21 +0100

    created measurement controllre and added the create show and index method

---
+++ app/controllers/measurements_controller.rb
@@ -0,0 +1,37 @@
+class MeasurementsController < ApplicationController
+
+	def index
+		@measurements = Measurement.all
+		render :json {
+			status: 202,
+			measurements: @measurements
+		}
+	end
+
+	def show
+		@measurement = Measurement.find_by(id: params[:measurement][:id])
+		render :json {
+			status: 202,
+			measurement: @measurement
+		}
+	end
+
+	def create
+		@measurement = Measurement.new(
+			user_id: params[:measurement][:user_id],
+			item_id: params[:measurement][:item_id],
+			measurement: params[:measurement][:measurement]
+			)
+		if @measurement.valid?
+			@measurement.save
+			render :json {
+				status: :created,
+				measurement: @measurement
+			}
+		else
+			render :json {
+				status: 401,
+				error: @measurement.errors.full_messages
+			}
+	end
+end
