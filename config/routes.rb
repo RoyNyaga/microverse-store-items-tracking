802e5554df14c674a0693fb837dd8b24a8e8ca97
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-03 15:52:18 +0100

    added items, users and measurement resources to the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'statics#home'
+  resources :items
+  resources :users
+  resources :measurements
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
