ed72926d95705e37c617f015dd40dc8523df6f82
Roy Nyaga <nyagaandreroy@gmail.com>
2020-03-15 20:11:46 +0100

    added test for all measurement request

--- spec/endpoints/measurement_spec.rb
+++ spec/endpoints/measurement_spec.rb
@@ -16,5 +16,14 @@ RSpec.describe "Measurement", :type => :request do
 
   end
 
+  it "successfull creation of a measurement should return a status of created" do
+  	post "/measurements", :params => { :measurement => {:user_id => @user.id, :item_id => @item.id, :measurement => "50"} }
+    post "/measurements", :params => { :measurement => {:user_id => @user.id, :item_id => @item.id, :measurement => "50"} }
+    get "/measurements"
+    json = JSON.parse(response.body)
+    expect(json["measurements"].count).to eql(2)
+
+  end
+
 
 end
